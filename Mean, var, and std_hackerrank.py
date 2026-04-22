497f439a6f5987ec82a0a8ce8cf957bb6836e551
Sanjay M <sanjaymahesh16_bec27@mepcoeng.ac.in>
2026-04-22 14:01:37 +0530

    Create Mean, var, and std_hackerrank.py

---
+++ Mean, var, and std_hackerrank.py
@@ -0,0 +1,6 @@
+import numpy
+n, m = map(int, input().split())
+my_array = numpy.array([input().split() for _ in range(n)], int)
+print(numpy.mean(my_array, axis=1))
+print(numpy.var(my_array, axis=0))
+print(round(numpy.std(my_array), 11))
